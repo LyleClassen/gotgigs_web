daaf078558edfa20bc663b65a5b0a6fc66ee5637
Lyle Classen <lylec@globalkinetic.com>
2019-07-26 14:41:30 +0200

    completed buttons

--- src/components/inputs/Button/index.jsx
+++ src/components/inputs/Button/index.jsx
@@ -1,39 +1,87 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import MuiButton from '@material-ui/core/Button';
-import { colorText, colorBackground } from 'styles/theme-utils';
+import theme from 'styles/theme.json';
+import {
+  colorText,
+  colorBackground,
+  colorPrimary,
+  colorSecondary,
+} from 'styles/theme-utils';
 import { determineVariant } from './utils';
 
-const useStyles = makeStyles(theme => ({
-  outlined: {
-    border: `1px solid ${colorText()({ theme })}`,
-    '&:hover': {
-      backgroundColor: colorText()({ theme }),
-      color: colorBackground()({ theme }),
-    },
-  },
-}));
+const themeOverrides = rounded => {
+  const baseTheme = createMuiTheme();
+  const mainTextColor = colorText()({ theme });
+  const mainPrimaryColor = colorPrimary()(theme);
+  const mainSecondaryColor = colorSecondary()(theme);
 
-const Button = ({ children, outlined, ...props }) => {
-  const classes = useStyles();
-  return (
+  return createMuiTheme({
+    ...theme,
+    overrides: {
+      MuiButton: {
+        root: {
+          borderRadius: rounded ? '4rem' : baseTheme.shape.borderRadius,
+        },
+        outlined: {
+          border: `1px solid ${mainTextColor}`,
+          '&:hover': {
+            backgroundColor: mainTextColor,
+            color: colorBackground()({ theme }),
+          },
+        },
+        containedPrimary: {
+          background: mainPrimaryColor,
+          backgroundImage: `linear-gradient(90deg,${mainPrimaryColor},${mainSecondaryColor})`,
+        },
+      },
+    },
+  });
+};
+
+const determineColor = (primary, secondary) => {
+  if (primary) {
+    return 'primary';
+  }
+  if (secondary) {
+    return 'secondary';
+  }
+  return 'default';
+};
+
+const Button = ({
+  children,
+  outlined,
+  rounded,
+  primary,
+  secondary,
+  ...props
+}) => (
+  <ThemeProvider theme={themeOverrides(rounded)}>
     <MuiButton
-      classes={classes}
       variant={determineVariant(outlined)}
+      color={determineColor(primary, secondary)}
       {...props}
     >
       {children}
     </MuiButton>
-  );
-};
+  </ThemeProvider>
+);
 
 Button.defaultProps = {
   outlined: undefined,
+  rounded: undefined,
+  primary: undefined,
+  secondary: undefined,
 };
 
 Button.propTypes = {
   outlined: PropTypes.bool,
+  rounded: PropTypes.bool,
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
   children: PropTypes.node.isRequired,
 };
 
--- src/styles/theme-utils.js
+++ src/styles/theme-utils.js
@@ -4,3 +4,9 @@ export const colorBackground = (variant = 'default') => props => {
 
 export const colorText = (variant = 'primary') => props =>
   props.theme.palette.text[variant];
+
+export const colorPrimary = (variant = 'main') => theme =>
+  theme.palette.primary[variant];
+
+export const colorSecondary = (variant = 'main') => theme =>
+  theme.palette.secondary[variant];
